8ad7f1678275e8bf915a7e10250f3ad603e845f7
Matu <36888936+Matu94@users.noreply.github.com>
2025-11-23 17:50:16 +0100

    syntax

--- Snowflake/02_Table/RAW/MEETINGS.sql
+++ Snowflake/02_Table/RAW/MEETINGS.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS RAW.MEETINGS (
 	RAW_PAYLOAD VARIANT,
 	LOADED_AT TIMESTAMP_NTZ(9) DEFAULT CURRENT_TIMESTAMP()
-	COMMENT = 'F1 project: Store all the data from the API endpoint: https://api.openf1.org/v1/meetings?';
-);
+)
+COMMENT = 'F1 project: Store all the data from the API endpoint: https://api.openf1.org/v1/meetings?';
