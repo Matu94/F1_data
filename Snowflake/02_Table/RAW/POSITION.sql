7351f093aed63fff9cd44be0d872d844a7dfd60a
Matu <36888936+Matu94@users.noreply.github.com>
2025-11-23 18:14:43 +0100

    Create POSITION.sql

---
+++ Snowflake/02_Table/RAW/POSITION.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS RAW.POSITION (
+	RAW_PAYLOAD VARIANT,
+	LOADED_AT TIMESTAMP_NTZ(9) DEFAULT CURRENT_TIMESTAMP()
+)
+COMMENT = 'F1 project: Store all the data from the API endpoint: https://api.openf1.org/v1/position';
