117f0da7b251120eb410a1503294623807b59d25
Matu <36888936+Matu94@users.noreply.github.com>
2025-12-10 15:20:09 +0100

    Create V_POSITIONS.sql

---
+++ Snowflake/03_VIEW/RAW/V_POSITIONS.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW RAW.V_POSITIONS_PARSED AS
+SELECT
+    RAW_PAYLOAD:"date"::TIMESTAMP_TZ      AS date,
+    RAW_PAYLOAD:"driver_number"::INT      AS driver_number,
+    RAW_PAYLOAD:"meeting_key"::INT        AS meeting_key,
+    RAW_PAYLOAD:"position"::INT           AS position,
+    RAW_PAYLOAD:"session_key"::INT        AS session_key,
+    LOADED_AT
+FROM RAW.POSITIONS;
